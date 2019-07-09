4094eaf65ebbc66b4f6cfbe4a46985f5cd9dde0e
Carlo Pantaleo <carlo.pantaleo@icloud.com>
2019-07-09 11:05:30 +0200

    Add fill character

--- Sba.DotNetUtils.Tabulate/TabulateStringExt.cs
+++ Sba.DotNetUtils.Tabulate/TabulateStringExt.cs
@@ -2,14 +2,14 @@
 
 namespace Sba.DotNetUtils.Tabulate {
     public static class TabulateStringExt {
-        public static string Tabulate(this string str, int length) {
+        public static string Tabulate(this string str, int length, char fill = ' ') {
             int absLength = Math.Abs(length);
 
             return length == 0
                 ? ""
                 : (length > 0
-                    ? str.PadLeft(absLength)
-                    : str.PadRight(absLength)).Substring(0, absLength);
+                    ? str.PadLeft(absLength, fill)
+                    : str.PadRight(absLength, fill)).Substring(0, absLength);
         }
     }
 }
