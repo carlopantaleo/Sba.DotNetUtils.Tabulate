afef29e28fc8fb51b5843abda0b234478f26d420
Carlo Pantaleo <carlo.pantaleo@icloud.com>
2019-07-09 10:04:46 +0200

    First commit

---
+++ Sba.DotNetUtils.Tabulate/TabulateStringExt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Sba.DotNetUtils.Tabulate {
+    public static class TabulateStringExt {
+        public static string Tabulate(this string str, int length) {
+            int absLength = Math.Abs(length);
+
+            return length == 0
+                ? ""
+                : (length > 0
+                    ? str.PadLeft(absLength)
+                    : str.PadRight(absLength)).Substring(0, absLength);
+        }
+    }
+}
