a94b881b441bc2be7be4200c8e92885118720ad3
Carlo Pantaleo <carlo.pantaleo@icloud.com>
2019-07-09 11:07:39 +0200

    Add unit tests

---
+++ Sba.DotNetUtils.Tabulate_UT/Tests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using Sba.DotNetUtils.Tabulate;
+
+namespace Sba.DotNetUtils.Tabulate_UT {
+    [TestFixture]
+    public class Tests {
+        [Test]
+        public void TabulateLeftAlign() {
+            Assert.AreEqual("a   ", "a".Tabulate(-4));
+        }
+        
+        [Test]
+        public void TabulateRightAlign() {
+            Assert.AreEqual("   a", "a".Tabulate(4));
+        }
+        
+        [Test]
+        public void TabulateLeftAlignFillWithPeriods() {
+            Assert.AreEqual("a...", "a".Tabulate(-4, '.'));
+        }
+    }
+}
